4075e3d3e55c30c224722f75f9864732ec0a4a65
Qihang Hu <qihanghu@foxmail.com>
2024-01-27 12:10:04 +0800

    feat: Create go.yml

--- example/main.go
+++ example/main.go
@@ -4,25 +4,33 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/A11Might/lark_docx_md"
 	lark "github.com/larksuite/oapi-sdk-go/v3"
 )
 
 func main() {
-	client := lark.NewClient("appId", "appSecret")
-	md, err := lark_docx_md.DocxMarkdown(context.Background(), client, "U3hXdQmMAoiNVSxDgPOcu4R8nTd")
+	client := lark.NewClient(os.Getenv("APP_ID"), os.Getenv("APP_SECRET"))
+	md, err := lark_docx_md.DocxMarkdown(context.Background(), client, os.Getenv("DOCUMENT_ID"))
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	f, err := os.Create("example.md")
+	filename := "dist/README.md"
+	_ = os.MkdirAll(filepath.Dir(filename), 0o755)
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0o666)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Printf("open file %s fail: %s", filename, err)
 		return
 	}
 	defer f.Close()
 	if _, err := f.WriteString(md); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	if err = os.Rename("static", "dist/static"); err != nil {
 		fmt.Println(err.Error())
 		return
 	}
